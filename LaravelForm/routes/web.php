2251a8cc303251ccae8e00b423ad83f09ddbc4cf
dominicaard <dominicaardhiniaa@gmail.com>
2022-11-24 00:22:37 +0700

    Update tbl&crud

--- LaravelForm/routes/web.php
+++ LaravelForm/routes/web.php
@@ -1,6 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\MahasiswaController;
+use App\Models\Post;
+use App\Models\Mahasiswa;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,11 +16,17 @@ use App\Http\Controllers\PostController;
 |
 */
 Route::get('/', function () {
-    return view('form');
+    return view('input-mahasiswa');
 });
-Route::get('add-blog-post-form', [PostController::class, 'index']);
 Route::post('store-form', [PostController::class, 'store']);
 Route::get('/read',[PostController::class,'read']);
 Route::get('/edit/{id}', [PostController::class, 'edit']);
 Route::get('/delete/{id}', [PostController::class, 'destroy']);
 Route::put('/update/{id}', [PostController::class, 'update']);
+
+Route::get('input-mahasiswa', [MahasiswaController::class,'index']);
+Route::post('kirim-mahasiswa', [MahasiswaController::class, 'store']);
+Route::get('delete-mahasiswa/{nim}',[MahasiswaController::class,'delete']);
+Route::get('edit-mahasiswa/{nim}',[MahasiswaController::class,'edit']);
+Route::get('show-data-mahasiswa',[MahasiswaController::class,'show']);
+Route::post('update-mahasiswa/{nim}',[MahasiswaController::class,'update']);
